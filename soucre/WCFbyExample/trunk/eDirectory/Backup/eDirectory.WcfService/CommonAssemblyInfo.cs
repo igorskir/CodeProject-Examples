2317f4a85ce094456afcfe98c4b9165c38c1c6af
Igor Skirnevskiy <igorskir@yandex.ru>
2016-02-15 08:54:38 +0600

    Проекты по папкам

--- soucre/WCFbyExample/trunk/eDirectory/Backup/eDirectory.WcfService/CommonAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyCompanyAttribute("WCF by Example")]
-[assembly: AssemblyProductAttribute("eDirectory")]
-[assembly: AssemblyCopyrightAttribute("WCF by Example - 2013")]
-[assembly: AssemblyTrademarkAttribute("")]
-[assembly: AssemblyVersionAttribute("1.0.3.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.3.0")]
